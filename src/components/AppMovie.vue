ce8c2fdc300184ee6b105a8946cc155947899a33
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-04-05 16:26:12 +0200

    flags added

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -2,7 +2,9 @@
     <li>
         {{ movie.title }}
         {{ movie.original_title }}
-        {{ movie.original_language }}
+        <img :src="getImageUrl(`../assets/flags/${movie.original_language}.png`)"
+            v-if="myFlags.includes(movie.original_language)" class="flag-leng" />
+        <span v-else> {{ movie.original_language }}</span>
         {{ movie.vote_average }}
     </li>
 </template>
@@ -12,8 +14,22 @@ export default {
     name: "AppMovie",
     props: {
         movie: Object
+    },
+    data() {
+        return {
+            myFlags: ['en', 'it']
+        }
+    },
+    methods: {
+        getImageUrl(path) {
+            return new URL(path, import.meta.url).href
+        }
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flag-leng {
+    width: 20px;
+}
+</style>
